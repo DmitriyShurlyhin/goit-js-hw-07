6167bf23cc96e4d9d046c92e7cb27469dc00c7c8
Dmitriy Shurlyhin <shurlygin91@gmail.com>
2021-09-09 16:32:14 +0300

    task 7 complete

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const spanText = document.querySelector('#text');
+const inputEl = document.querySelector('#font-size-control');
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+  spanText.style.fontSize = `event.currentTarget.value + 'px'`;
+}
